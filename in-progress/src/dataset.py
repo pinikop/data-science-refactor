20300f527b00d6f17692d61f0fc2104bb2f225e7
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-05 22:37:34 +0200

    refactor dataset

--- in-progress/src/dataset.py
+++ in-progress/src/dataset.py
@@ -2,21 +2,21 @@ from pathlib import Path
 from typing import Any
 
 import numpy as np
+import numpy.typing as npt
 import torch
 from src.load_data import load_image_data, load_labels
 from torch.utils.data import DataLoader, Dataset
 
 
 class MNIST(Dataset):
-    idx: int  # requested data index
     x: torch.Tensor
     y: torch.Tensor
 
     TRAIN_MAX = 255.0
-    TRAIN_NORMALIZED_MEAN = 0.1306604762738429
-    TRAIN_NORMALIZED_STDEV = 0.3081078038564622
+    MU = 0.1306604762738429
+    STD = 0.3081078038564622
 
-    def __init__(self, data: np.ndarray, targets: np.ndarray):
+    def __init__(self, data: npt.NDArray, targets: npt.NDArray):
         if len(data) != len(targets):
             raise ValueError('data and targets must be the same length. '
                              f'{len(data)} != {len(targets)}')
@@ -28,32 +28,21 @@ class MNIST(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx) -> tuple[torch.Tensor, torch.Tensor]:
-        x = self.get_x(idx)
-        y = self.get_y(idx)
+        x = self._get_x(idx)
+        y = self._get_y(idx)
         return x, y
 
-    def get_x(self, idx: int):
-        self.idx = idx
-        self.preprocess_x()
-        return self.x
+    def _get_x(self, idx: int):
+        x = self.data[idx].astype(np.float32)
+        x /= self.TRAIN_MAX
+        x = (x - self.MU) / self.STD
+        x = torch.from_numpy(x)
+        x = x.unsqueeze(0)
+        return x
 
-    def preprocess_x(self):
-        self.x = self.data[self.idx].copy().astype(np.float64)
-        self.x /= self.TRAIN_MAX
-        self.x -= self.TRAIN_NORMALIZED_MEAN
-        self.x /= self.TRAIN_NORMALIZED_STDEV
-        self.x = self.x.astype(np.float32)
-        self.x = torch.from_numpy(self.x)
-        self.x = self.x.unsqueeze(0)
-
-    def get_y(self, idx: int):
-        self.idx = idx
-        self.preprocess_y()
-        return self.y
-
-    def preprocess_y(self):
-        self.y = self.targets[self.idx]
-        self.y = torch.tensor(self.y, dtype=torch.long)
+    def _get_y(self, idx: int):
+        y = self.targets[idx]
+        return torch.tensor(y, dtype=torch.long)
 
 
 def create_dataloader(
