1f23344d92171e022395183d7aea707f8b564b61
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-05 09:02:40 +0200

    wrap network in Sequential

--- in-progress/src/models.py
+++ in-progress/src/models.py
@@ -5,16 +5,13 @@ class LinearNet(torch.nn.Module):
     def __init__(self):
         super(LinearNet, self).__init__()
 
-        self.flatten = torch.nn.Flatten()
-        self.linear1 = torch.nn.Linear(in_features=28 * 28, out_features=32)
-        self.relu = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(in_features=32, out_features=10)
-        self.softmax = torch.nn.Softmax(dim=1)
+        self.netweork = torch.nn.Sequential(
+            torch.nn.Flatten(),
+            torch.nn.Linear(in_features=28 * 28, out_features=32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=32, out_features=10),
+            torch.nn.Softmax(dim=1)
+        )
 
     def forward(self, x: torch.Tensor):
-        x = self.flatten(x)
-        x = self.linear1(x)
-        x = self.relu(x)
-        x = self.linear2(x)
-        x = self.softmax(x)
-        return x
+        return self.netweork(x)
